63ab12c7adbd9081764f9a9852a30dabf97252b9
Iosif <dev@iosifv.com>
2024-10-06 23:43:37 +0100

    [main] - rename

---
+++ java/fcc-runnerz/src/main/java/com/iosifv/runnerz/run/RunRepository.java
@@ -0,0 +1,30 @@
+package com.iosifv.runnerz.run;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class RunRepository {
+
+    private static final Logger log = LoggerFactory.getLogger(RunRepository.class);
+    private final JdbcClient jdbcClient;
+
+    public RunRepository(final JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    public List<Run> findAll() {
+        return jdbcClient.sql("SELECT * FROM Run")
+                .query(Run.class)
+                .list();
+    }
+}
---
+++ java/fcc-runnerz/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO Run (id, title, started_on, completed_on, miles, location)
+VALUES (1, 'Morning Run', '2024-10-05 06:30:00', '2024-10-05 07:00:00', 5, 'Park');
+
+INSERT INTO Run (id, title, started_on, completed_on, miles, location)
+VALUES (2, 'Evening Run', '2024-10-05 18:15:00', '2024-10-05 19:00:00', 8, 'Track');
+
+INSERT INTO Run (id, title, started_on, completed_on, miles, location)
+VALUES (3, 'Weekend Trail', '2024-10-06 08:00:00', '2024-10-06 09:30:00', 10, 'Trail');
+
+INSERT INTO Run (id, title, started_on, completed_on, miles, location)
+VALUES (4, 'City Marathon', '2024-09-20 09:00:00', '2024-09-20 12:45:00', 26, 'City');
+
+INSERT INTO Run (id, title, started_on, completed_on, miles, location)
+VALUES (5, 'Night Run', '2024-10-04 22:00:00', '2024-10-04 23:00:00', 7, 'Beach');
